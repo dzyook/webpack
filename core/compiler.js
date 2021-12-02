f88a7da544f81d92f3b62bf3c869c1630ac09087
dengzy <dengzy@shanzhen.com>
2021-12-02 19:41:16 +0800

    update

--- core/compiler.js
+++ core/compiler.js
@@ -45,7 +45,6 @@ class Compiler {
     this.buildEntryModule(entry);
     // 导出列表;之后将每个chunk转化称为单独的文件加入到输出列表assets中
     this.exportFile(callback);
-    console.log(this.assets, 'assets')
   }
 
   // 将chunk加入输出列表中去
